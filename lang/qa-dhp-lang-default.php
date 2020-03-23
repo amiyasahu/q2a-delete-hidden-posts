64b1888c1a6fe8bcef5f141c2bb768e451730f18
Amiya Sahu <amiyasahu@users.noreply.github.com>
2020-03-23 15:00:54 -0400

    Update qa-dhp-lang-default.php

--- lang/qa-dhp-lang-default.php
+++ lang/qa-dhp-lang-default.php
@@ -16,7 +16,7 @@
         'delete_q'                  => 'Delete',
         'delete_a'                  => 'Delete',
         'delete_c'                  => 'delete',
-        'are_you_sure'              => 'Are you sure ?',
+        'are_you_sure'              => 'Are you sure?',
     );
 
     /*
